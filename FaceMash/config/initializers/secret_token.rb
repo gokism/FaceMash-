ad40428f79dcadcec33b61c063c1ea8a5054df1f
gokul <gokul.dynamite@gmail.com>
2013-05-03 18:22:31 +0530

    INITIAL COMMIT

---
+++ FaceMash/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaceMash::Application.config.secret_token = 'd796ac366b94768a0b17f25f70bcb9052007313650bb0409f361068457bf9c3b60641e2c94722fb47f07ee25436176fb8b4657a23c3d1ac2a029672036c58c17'
