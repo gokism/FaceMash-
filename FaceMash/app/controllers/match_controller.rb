ccaae6ad0362006332f7ff01ab52984fb9692ac1
gokul <gokul.dynamite@gmail.com>
2013-05-27 17:01:16 +0530

    Fixed the opppenents algo

--- FaceMash/app/controllers/match_controller.rb
+++ FaceMash/app/controllers/match_controller.rb
@@ -1,8 +1,21 @@
 class MatchController < ApplicationController
   def start
 
-    @girl_one=Girl.order("rating ASC").first
-    @girl_two=Girl.order("rating ASC").second
+    @top_girls=Girl.order("rating DESC LIMIT 5")
+    @girl_one=Girl.order("total_matches ASC").first
+    girl_twos=Girl.where("id!=?",@girl_one.id)
+    all_played_once=nil
+    girl_twos.each do |girl|
+      match=Match.where("girl_one=? AND girl_two=? OR girl_one=? AND girl_two=?",@girl_one.id,girl.id,girl.id,@girl_one.id).first
+    if(match==nil && Match.create(:girl_one=>@girl_one.id,:girl_two=>girl.id))
+      @girl_two=girl
+      break
+    else
+      all_played_once=1
+    end
+    end
+    
+    
     @girl_one_won="./end_match?girl_won=#{@girl_one.id}&girl_lost=#{@girl_two.id}"
     @girl_two_won="./end_match?girl_lost=#{@girl_one.id}&girl_won=#{@girl_two.id}"
 
@@ -13,11 +26,12 @@ class MatchController < ApplicationController
     girl_won = Girl.find_by_id(params[:girl_won])
     girl_lost = Girl.find_by_id(params[:girl_lost])
     girl_won_total_matches = girl_won.total_matches + 1
-    girl_lost_total_mathces=girl_lost.total_matches + 1
+    girl_lost_total_matches=girl_lost.total_matches + 1
     girl_won_matches_won=girl_won.matches_won + 1
     girl_lost_matches_lost=girl_lost.matches_lost + 1
+    
 
-    if girl_won.update_attributes(:total_matches=>girl_won_total_matches,:matches_won=>girl_won_matches_won) && girl_lost.update_attributes(:total_matches=>girl_won_total_matches,:matches_lost=>girl_lost_matches_lost)
+    if girl_won.update_attributes(:total_matches=>girl_won_total_matches,:matches_won=>girl_won_matches_won) && girl_lost.update_attributes(:total_matches=>girl_lost_total_matches,:matches_lost=>girl_lost_matches_lost)
 
       if Match.where("girl_one=? AND girl_two=?",girl_won.id,girl_lost.id).first==nil && Match.where("girl_one=? AND girl_two=?",girl_lost.id,girl_won.id).first==nil
         if(Match.create(:girl_one=>girl_won.id,:girl_two=>girl_lost.id))
